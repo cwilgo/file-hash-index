b911a06feb1ca04ea705fa73464df7db34029c64
Wil Tetz <wil@alpinecomputence.com>
2019-05-15 12:07:38 -0600

    Add project files

---
+++ Item.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace md5db
+{
+    public class Item
+    {
+        public string MD5 { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ Options.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace md5db
+{
+    public class Options
+    {
+        public Options()
+        {
+            BasePath = System.IO.Directory.GetCurrentDirectory();
+            HashFilename = "hash.md5";
+        }
+
+        public Options(string[] args)
+            : this()
+        {
+            ProcessCommandParameters(args);
+        }
+
+        public string BasePath { get; private set; }
+        public string HashFilename { get; private set; }
+        public bool DisplayHelp { get; private set; }
+
+        public void ProcessCommandParameters(string[] args)
+        {
+            foreach (var item in args)
+            {
+                switch (item)
+                {
+                    case "--help":
+                    case "-h":
+                        DisplayHelp = true;
+                        break;
+                }
+            }
+        }
+
+    }
+}
